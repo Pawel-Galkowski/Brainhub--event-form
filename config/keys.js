ca324620aa188b9119f0cbe355bc55995e43e78a
pawel-galkowski <p.galkowski@ik.pl>
2020-08-17 14:26:46 +0200

    keys update

--- config/keys.js
+++ config/keys.js
@@ -1,9 +1,15 @@
-﻿module.exports = {
-    mongoURI: "mongodb+srv://brainhub-admin:ydB9rFGGxpwMm53k@cluster0.ltiay.mongodb.net/brainhub?retryWrites=true&w=majority",
-    options:{
-        useUnifiedTopology: true,
-        useNewUrlParser: true,
-        useFindAndModify: false
-    }
-}
+﻿var url =
+  "mongodb+srv://" +
+  process.env.DATABASE_LOGIN +
+  ":" +
+  process.env.DATABASE_PASSWORD +
+  "@cluster0.ltiay.mongodb.net/brainhub?retryWrites=true&w=majority";
 
+module.exports = {
+  mongoURI: url,
+  options: {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useFindAndModify: false,
+  },
+};
